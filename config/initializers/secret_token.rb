95cfc5c14433ada23fe04b7c0cf17bfb780d9061
Thomas Junghans <thomas.junghans@gmail.com>
2014-05-05 18:38:21 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inanasite::Application.config.secret_token = '285f973862bd91300b52f2fb76d2e6e07a2e695e79eef3140bc63593e76ffef6604564ac42a15b81706419d007e764efd5953bc55f2a745aad6bd205a5a859b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Inanasite::Application.config.session_store :cookie_store, key: '_inanasite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Inanasite::Application.config.session_store :active_record_store
