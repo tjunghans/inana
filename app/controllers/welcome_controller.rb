9d58a8d7a1ed639a35d35bd54b28efadec3da6cc
Thomas Junghans <thomas.junghans@gmail.com>
2014-05-12 21:21:15 +0200

    Add facebook meta

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-
+    @siteTitle = "Inana Music"
+    @thumbUrl = "http://www.inana.co.za/assets/inana-twitter.png"
+    @siteUrl = "http://www.inana.co.za"
+    @siteDescription = "Online profile of Inana, south african singer, song-writer, actress, voice over artist and public speaker"
   end
 end
